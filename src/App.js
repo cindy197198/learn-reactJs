b28bb525c7a4fbc800b6e7f84051a7ef3d2cfe02
cindy197198 <trangnguyen197198@gmail.com>
2020-07-12 21:47:12 +0700

    render a list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      { title: 'Learn React', isCompleted: true },
+      { title: 'Cooking', isCompleted: true },
+      { title: 'Watch film', isCompleted: false },
+      { title: 'Go to the movies', isCompleted: true }
+    ];
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item, index) => 
+          <TodoItem key={index} item={item}></TodoItem>)
+        }
+      </div>
+    );
+  }
+  
 }
 
 export default App;
