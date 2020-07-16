9fd34bf17299177b34b7d3c71c53dc77ebeaa667
cindy197198 <trangnguyen197198@gmail.com>
2020-07-16 13:58:09 +0700

    counter app - component lifecycles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TodoItem from './components/TodoItem';
-import Calculator from './components/Calculator';
-import tick from './img/tick.svg';
-
+import Counter from './components/Counter';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.inputElement = React.createRef();
+    this.state = {
+      showCounter: true
+    };
+  }
+  componentDidUpdate() {
+    console.log('App updated');
   }
 
-  componentDidMount() {
-    this.inputElement.current.focus()
+  removeCounter() {
+    this.setState({
+      showCounter: false
+    });
   }
-
   render() {
+    console.log('App render');
       return (
         <div className="App">
-          <input type="text" ref={this.inputElement} />
+          <button onClick={() => this.removeCounter()}>Remove Counter</button>
+          {this.state.showCounter && <Counter />}
         </div>
       );
   }
