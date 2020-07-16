c6e076992f6b45b203382e435db8feb9ef0476b5
cindy197198 <trangnguyen197198@gmail.com>
2020-07-16 13:17:17 +0700

    createRef in react

--- src/App.js
+++ src/App.js
@@ -7,76 +7,19 @@ import tick from './img/tick.svg';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      newItem: '',
-      todoItems: [
-        { title: 'Learn React', isCompleted: true },
-        { title: 'Cooking', isCompleted: true },
-        { title: 'Watch film', isCompleted: false },
-        { title: 'Go to the movies', isCompleted: true }
-      ]
-    }
-    this.onKeyUp = this.onKeyUp.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
-  onItemClick(item) {
-    return ( (event) => {
-      const isComplete = item.isCompleted;
-      const todoItems = this.state.todoItems;
-      const index = todoItems.indexOf(item);
-      this.setState({
-        todoItems:[
-          ...todoItems.slice(0, index),
-          {
-            ...item,
-            isCompleted: !isComplete
-          },
-          ...todoItems.slice(index+1)
-        ]
-      });
-    }
-    );
+  constructor(props) {
+    super(props);
+    this.inputElement = React.createRef();
   }
 
-  onKeyUp(event) {
-    let text = event.target.value;
-    if (event.keyCode === 13) { //phim Enter
-      text = text.trim();
-      if (!text) { return; }
-      this.setState({
-        newItem: '',
-        todoItems: [
-          { title: text, isCompleted: false },
-          ...this.state.todoItems
-        ]
-      });
-    }
+  componentDidMount() {
+    this.inputElement.current.focus()
   }
 
-  onChange(event) {
-    this.setState({
-      newItem: event.target.value
-    });
-  }
   render() {
-    const {todoItems, newItem} = this.state;
       return (
         <div className="App">
-          <div className="header">
-            <img src={tick} width={32} height={32} />
-            <input type="text" placeholder="Add a new item"
-                value={newItem}
-                onChange={this.onChange}
-                onKeyUp={this.onKeyUp}
-            />
-
-          </div>
-          {
-            todoItems.length && todoItems.map((item, index) =>
-            <TodoItem key={index} item={item} onClick={this.onItemClick(item)}></TodoItem>)
-          }
+          <input type="text" ref={this.inputElement} />
         </div>
       );
   }
